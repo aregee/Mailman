cdee94fd33c9240117a21561825b32f2d79c808d
Rahul Gaur <rahul.nbg@gmail.com>
2013-04-30 18:45:57 +0530

    removed *~ files from repo

--- restfm/src/restfm/views.py
+++ restfm/src/restfm/views.py
@@ -1,30 +1,10 @@
 # Create your views here.
-from restfm.models import *
 from restfm.serializers import *
 from rest_framework import generics 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.conf import settings
-from django.contrib.auth.models import User
-from django.db.models.signals import pre_delete, pre_save
-from django.db import models
-from django.dispatch import receiver
-from django.http import Http404
 from mailmanclient import Client, MailmanConnectionError
-from social_auth.signals import socialauth_registered
-from urllib2 import HTTPError
-
-import os
-
-import time
-
-import subprocess
-
-from mocker import Mocker
-
-mocker = Mocker()
-
-from mailmanclient import Client 
 
 
 client = Client('%s/3.0' % settings.REST_SERVER, settings.API_USER, settings.API_PASS)
